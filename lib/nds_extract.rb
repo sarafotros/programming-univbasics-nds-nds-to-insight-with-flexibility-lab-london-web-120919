d4a82995aa31976c16836185f02edfbc96656097
Sara Fotros <sarafotros1@yahoo.com>
2019-11-14 18:17:07 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,7 +32,8 @@ end
 
 
 # Your code after this point
-
+require 'pp'
+pp aoa
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
